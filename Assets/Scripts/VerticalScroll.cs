19676539d36d7e7465248a2c884d156f9ac72b4e
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-03-22 12:18:59 +0100

    Last commits

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+	//Parameters
+	[SerializeField] float scrollSpeed;
+	
+	void Update()
+	{
+		ScrollUpwards();
+	}
+
+	private void ScrollUpwards()
+	{
+		transform.Translate(Vector2.up * Time.deltaTime * scrollSpeed);
+	}
+}
